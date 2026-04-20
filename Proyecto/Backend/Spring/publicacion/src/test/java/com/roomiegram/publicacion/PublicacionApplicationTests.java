c64e87081b0c0fcccea8865396310fda51b45df5
Juanjo Barber <ju.barber@duocuc.cl>
2026-04-19 23:52:46 -0400

    Creacion de microservicio de publicacion
    Creacion del microservicio para publicaciones del sistema, encargado de gestionar las publicaciones de hogares

---
+++ Proyecto/Backend/Spring/publicacion/src/test/java/com/roomiegram/publicacion/PublicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomiegram.publicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

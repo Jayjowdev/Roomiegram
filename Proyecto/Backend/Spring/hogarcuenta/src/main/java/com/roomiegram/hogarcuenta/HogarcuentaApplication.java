b046aa8ad9482141a2c561d77bffc6b2f94f69d9
Juanjo Barber <ju.barber@duocuc.cl>
2026-04-20 01:50:49 -0400

    Creacion del microservicio hogarcuenta

---
+++ Proyecto/Backend/Spring/hogarcuenta/src/main/java/com/roomiegram/hogarcuenta/HogarcuentaApplication.java
@@ -0,0 +1,13 @@
+package com.roomiegram.hogarcuenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogarcuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogarcuentaApplication.class, args);
+	}
+
+}
---
+++ Proyecto/Backend/Spring/hogarcuenta/src/test/java/com/roomiegram/hogarcuenta/HogarcuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomiegram.hogarcuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogarcuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
